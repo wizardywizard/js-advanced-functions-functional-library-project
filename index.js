59e5e59f7e4163a280a70240f9687a12a76c9bb1
wizardywizard <littledavid175@gmail.com>
2020-03-01 16:20:55 -0800

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,55 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, alert) {
+      if (Array.isArray(collection)){
+        for (let i = 0; i < collection.length; i++){
+          alert(collection[i])
+        }          
+      }else{
+        var collect = Object.values(collection)
+        for (let i = 0; i < collect.length; i++){
+          alert(collect[i])
+        }
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      var answer = []
+      if (Array.isArray(collection)){
+        for (let i = 0; i < collection.length; i++){
+          answer.push(callback(collection[i]))
+        }
+      }else{
+        var collect = Object.values(collection)
+        for (let i = 0; i < collect.length; i++){
+          answer.push(callback(collect[i]))
+        }
+      }
+      return answer
     },
 
-    reduce: function() {
+    reduce: function(collection, callback, acc) {
+      let answer = 0
+      let i = 0
+      if (acc){
+       answer = acc
+      }else{
+        answer = collection[0]
+        i = 1
+      }      
+        for (i ; i < collection.length; i++){
+          answer = callback(answer, collection[i])
+        }
+      return answer
+    },
 
+    find: function(collection, predicate){
+      for (let i = 0; i < collection.length; i++){
+        var answer = predicate(collection[i])
+      }
+      console.log(answer)
     },
 
     functions: function() {
